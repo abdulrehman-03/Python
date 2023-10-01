cf0a10fd83a267189ffe3be6a6d5bdcaebe08f0b
Abdulrehman <76557290+abdulrehman-03@users.noreply.github.com>
2023-10-01 14:44:06 +0500

    Update random_jimmy.py

--- Projects/turtle graphics/random_jimmy.py
+++ Projects/turtle graphics/random_jimmy.py
@@ -1,11 +1,19 @@
+import turtle
 from turtle import Turtle, Screen
 import random
 
+turtle.colormode(255)
 jimmy = Turtle()
 screen = Screen()
 
-colors = ["CornflowerBlue", "DarkRed", "DarkOrchid3", "DarkSlateGray2", "gray40", "gold", "navy", "LightSlateBlue",
-          "PaleGreen1"]
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    colors = (r, g, b)
+    return colors
+
 
 jimmy.pensize(7)
 jimmy.speed(10)
@@ -14,7 +22,7 @@ directions = [0, 90, 180, 270]
 
 for _ in range(250):
     jimmy.forward(20)
-    jimmy.color(random.choice(colors))
+    jimmy.color(random_color())
     jimmy.setheading(random.choice(directions))
 
 screen.exitonclick()
