79f5c7838d67a4e653764b5397bfff5970a9d431
Abdulrehman <76557290+abdulrehman-03@users.noreply.github.com>
2023-10-04 23:51:31 +0500

    Update main.py

--- Projects/turtle graphics/snake-game/main.py
+++ Projects/turtle graphics/snake-game/main.py
@@ -1,14 +1,20 @@
-from turtle import Screen, Turtle
+from turtle import Screen
+from structure import Background
 from snake import Snake
+from food import Food
+from scoreboard import ScoreBoard
 import time
 
 screen = Screen()
-screen.setup(width=600, height=600)
+screen.setup(width=650, height=650)
 screen.bgcolor("black")
 screen.title("Snake Game")
 screen.tracer(0)
 
+boundary = Background()
 snake = Snake()
+food = Food()
+score = ScoreBoard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -21,5 +27,19 @@ while game_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
+
+    if snake.head.distance(food) < 15:
+        score.update()
+        food.refresh()
+        snake.extend()
+
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 275 or snake.head.ycor() < -280:
+        game_on = False
+        score.game_over()
+
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_on = False
+            score.game_over()
 
 screen.exitonclick()
