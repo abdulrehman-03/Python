55879cfe91d7d07f0b5c87b5c8085ac34c863503
Abdulrehman <76557290+abdulrehman-03@users.noreply.github.com>
2023-10-05 22:13:45 +0500

    Create main.py

---
+++ Projects/turtle graphics/pong-game/main.py
@@ -0,0 +1,44 @@
+from turtle import Screen, Turtle
+from paddles import Paddles
+from ball import Ball
+from scoreboard import Scoreboard
+import time
+
+
+screen = Screen()
+
+screen.bgcolor("black")
+screen.setup(height=600, width=800)
+screen.title("Pong")
+screen.tracer(0)
+
+ball = Ball()
+r_paddle = Paddles((350, 0))
+l_paddle = Paddles((-350, 0))
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkeypress(r_paddle.go_up, "Up")
+screen.onkeypress(r_paddle.go_down, "Down")
+screen.onkeypress(l_paddle.go_up, "w")
+screen.onkeypress(l_paddle.go_down, "s")
+
+game_on = True
+while game_on:
+    time.sleep(ball.move_speed)
+    screen.update()
+    ball.move()
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    if ball.xcor() > 320 and ball.distance(r_paddle) < 50 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
+
+screen.exitonclick()
