9d6095d137e966bed535bd148c6af999f0514feb
Abdulrehman <76557290+abdulrehman-03@users.noreply.github.com>
2023-10-17 23:08:39 +0500

    Create main.py

---
+++ Projects/nato alphabets/main.py
@@ -0,0 +1,15 @@
+import pandas
+
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
+
+phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+
+while True:
+    word = input("Enter a word: ").upper()
+    try:
+        phonetic_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+    else:
+        print(phonetic_list)
+        break
